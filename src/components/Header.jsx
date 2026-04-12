7db2db2fcbb08cf9c26e543b9ddf14319fdba6cf
ccristianysilva-cmd <ccristianysilva@gmail.com>
2026-04-12 16:52:46 -0300

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Cristiane</h1>
+            <h2>Estudante de Sistemas para Internet</h2>
+        </header>
+    );
+};
+
+export default Header;
