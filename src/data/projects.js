c8d83d77eac215a1168f3dff82c99681f1429e63
ccristianysilva-cmd <ccristianysilva@gmail.com>
2026-04-12 16:53:57 -0300

    Add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    name: "Projeto 1 - Página Web",
+    description: "Meu primeiro projeto em HTML e CSS",
+    tech: "HTML, CSS",
+    github: "#",
+    demo: "#"
+  },
+  {
+    id: 2,
+    name: "Projeto 2 - Aplicação JavaScript",
+    description: "Aplicação interativa com JavaScript",
+    tech: "JavaScript, HTML, CSS",
+    github: "#",
+    demo: "#"
+  },
+  {
+    id: 3,
+    name: "Projeto 3 - Componentes React",
+    description: "Aplicação com componentes React reutilizáveis",
+    tech: "React, JavaScript",
+    github: "#",
+    demo: "#"
+  }
+];
+
+export default projects;
